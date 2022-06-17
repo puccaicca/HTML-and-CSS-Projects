4ddeb7a661e66549f0083e659b44cf78136f3936
Lilian Tan <lilytan6007@gmail.com>
2022-06-16 22:45:57 -0700

    HTML and JavaScript folder
    Add assignments from HTML and JavaScript folder

---
+++ Basic_HTML_and_JavaScript_1/My_First_Function.js
@@ -0,0 +1,5 @@
+            function My_First_Function() {
+                var String = "Kiss me, I'm Irish!";
+                var result = String.fontcolor("red");
+                document.getElementById("Irish").innerHTML = result;
+            }
